ab6ec9aaf1ac57ce0965eea4b239552107255f71
Benedikt Werner <1benediktwerner@gmail.com>
2018-01-29 17:01:53 +0100

    Created dotnetcore project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WizzardExtreme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
